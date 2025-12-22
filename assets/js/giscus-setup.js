22ffaf8b8e35a76ad894c86a898842e5f0f1b96e
stpark7 <stpark5013@gmail.com>
2025-12-22 03:51:52 +0000

    Deploying to gh-pages from @ stpark7/stpark7.github.io@aed7b6369ed51a191e0ddff52fcff275bd2650cd 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "stpark7/stpark.github.io",
+    "data-repo": "stpark7/stpark7.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
